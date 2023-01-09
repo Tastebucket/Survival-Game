1a02e91726eef67d9a1dbabe21d095cffb182d3b
EljerPenny <elijahjpenny@gmail.com>
2023-01-09 06:04:58 -0500

    An ugly but essentially fully functional version (with all console.logs commented out)

--- js/app.js
+++ js/app.js
@@ -1,17 +1,71 @@
-console.log("It's go time")
+// console.log("It's go time")
 
+////Grabbing DOM elements
 const game = document.getElementById('canvas')
 const container = document.getElementById('container')
 const points = document.getElementById('points')
 const titleScreen = document.getElementById('title-screen')
 const startButton = document.getElementById('start-button')
+const instructionButton = document.getElementById('instructions')
+const highScoreButton = document.getElementById('high-scores')
 const levelDisplay = document.getElementById('level')
+const instructionScreen = document.getElementById('instruction-screen')
+const highScoreScreen = document.getElementById('high-score-screen')
+const gameOverScreen = document.getElementById('game-over-screen')
+const backButton = document.getElementById('back-button')
+const backButton2 = document.getElementById('back-button-2')
+const playAgain = document.getElementById('play-again')
+const theHighScore = document.getElementById('the-high-score')
+const finalScore = document.getElementById('final-score')
+const scoreboard = document.getElementById('scoreboard')
+
 const ctx = game.getContext('2d')
 
 game.setAttribute('width', getComputedStyle(game)['width'])
 game.setAttribute('height', getComputedStyle(game)['height'])
 game.height=600
 
+//////Title Screen Functions///////
+const startGame = () => {
+    titleScreen.style.display = 'none'
+    gameInterval = setInterval(gameLoop, 50)
+    smallEnemies.push(new Enemy(getRandomCoordinates(game.width)-30, getRandomCoordinates(game.height)-40, 30, 40, 'brown',(lvl+2),true))
+    setEnemyDirection()
+    releaseEnemy = setInterval(newEnemy,10000,(2+lvl))
+    releaseSeeker = setInterval(newSeeker,16000,lvl)
+    powerInterval = setInterval(createPower,18000)
+    enemyMove = setInterval(setEnemyDirection, 1300)
+    newLevel = setInterval(lvlUp,35000)
+    player.alive=true
+    player.x=game.width/2
+    player.y=game.height/2
+}
+const viewInstructions = () => {
+    titleScreen.style.display = 'none'
+    instructionScreen.style.display = 'initial'
+}
+const viewHighScores = () => {
+    titleScreen.style.display = 'none'
+    highScoreScreen.style.display = 'initial'
+}
+const mainMenu = () => {
+    titleScreen.style.display = 'initial'
+    instructionScreen.style.display = 'none'
+    highScoreScreen.style.display = 'none'
+    gameOverScreen.style.display = 'none'
+    point=0
+    smallEnemies = []
+    seekers = []
+    lvl=1
+}
+
+
+startButton.addEventListener('click',startGame)
+instructionButton.addEventListener('click',viewInstructions)
+highScoreButton.addEventListener('click',viewHighScores)
+backButton.addEventListener('click',mainMenu)
+backButton2.addEventListener('click',mainMenu)
+playAgain.addEventListener('click',mainMenu)
 
 class Enemy {
     constructor (x,y, width, height,color,speed,alive) {
@@ -35,7 +89,6 @@ class Enemy {
         this.setDirection = () => {
             //randomizes direction of travel
             const way = Math.floor(Math.random()*4)
-            // console.log(way)
             if (way == 0) {this.direction = {
                 up: true,
                 down: false,
@@ -244,8 +297,8 @@ const getRandomCoordinates = (max) => {
     return Math.floor(Math.random() * max)
 }
 const player = new User(game.width/2, game.height/2, 20, 20, 'purple')
-const smallEnemies = []
-const seekers = []
+let smallEnemies = []
+let seekers = []
 // const enemy = new Enemy(0, 0, 30, 40, 'brown',4,true)
 // const enemy2 = new Enemy(getRandomCoordinates(game.width), getRandomCoordinates(game.height), 30, 40, 'purple',8,true)
 // const enemy3 = new Enemy(getRandomCoordinates(game.width), getRandomCoordinates(game.height),40, 10, 'green',13, false)
@@ -255,20 +308,20 @@ const seekers = []
 //     setInterval(enemy3.setDirection,300)
 //     console.log('Yahoo!')
 // }
-const newEnemy = (rate) => {
+const newEnemy = () => {
     place = getRandomCoordinates(2)
     if (place===0) {
-        smallEnemies.push(new Enemy(0, 0, 30, 40, 'brown',rate,true))
+        smallEnemies.push(new Enemy(0, 0, 30, 40, 'brown',(lvl+2),true))
     } else {
-        smallEnemies.push(new Enemy((game.width-30), (game.height-40), 30, 40, 'brown',rate,true))
+        smallEnemies.push(new Enemy((game.width-30), (game.height-40), 30, 40, 'brown',(lvl+2),true))
     }
-    console.log('Hi')
-    console.log(smallEnemies)
+    // console.log('Hi')
+    // console.log(smallEnemies)
 }
-const newSeeker = (rate) => {
-    seekers.push(new Seeker(getRandomCoordinates(game.width), getRandomCoordinates(game.height), 30, 40, 'lightsteelblue',rate,true))
-    console.log('Gonna getcha')
-    console.log(seekers)
+const newSeeker = () => {
+    seekers.push(new Seeker(getRandomCoordinates(game.width)-20, getRandomCoordinates(game.height)-30, 20, 30, 'lightsteelblue',lvl,true))
+    // console.log('Gonna getcha')
+    // console.log(seekers)
 }
 
 const detectHit = (thing) => {
@@ -276,7 +329,7 @@ const detectHit = (thing) => {
         && player.x + player.width > thing.x
         && player.y < thing.y + thing.height
         && player.y + player.height > thing.y) {
-            console.log('HIT!')
+            // console.log('HIT!')
             player.alive = false
         }
 }
@@ -293,13 +346,29 @@ const detectHit = (thing) => {
         // nuke: clear all enemy arrays
 const powerArray = []
 class Power {
-    constructor (x,y,color,type) {
+    constructor (x,y) {
         this.x = x
         this.y = y
-        this.width = 30
-        this.height = 70
-        this.color = color
-        this.type = type
+        this.width = 25
+        this.height = 40
+        const rando = getRandomCoordinates(20)
+        // console.log(`This is the random number: ${rando}`)
+        if (rando<8){
+            this.color='green'
+            this.type='pointer'
+        } else if (rando<11) {
+            this.color="black"
+            this.type="stopper"
+        } else if (rando<14){
+            this.color = 'pink'
+            this.type = 'slowpoke'
+        } else if (rando<18) {
+            this.color = 'white'
+            this.type = 'speedy'
+        } else {
+            this.color = 'red'
+            this.type = 'destroyer'
+        }
         this.render = function (){
             ctx.fillStyle = this.color
             ctx.fillRect(this.x, this.y, this.width, this.height)
@@ -311,33 +380,37 @@ const resumeSpeed = (things, fast) => {
     for (let i = 0; i < things.length; i++) {
         things[i].speed = fast
     }
-    console.log("This is fast in resume", fast)
-    console.log("This is things in resume", things)
-    console.log('as you were')
+    // console.log("This is fast in resume", fast)
+    // console.log("This is things in resume", things)
+    // console.log('as you were')
 
 }
 const createPower = () => {
-    const rando = getRandomCoordinates(20)
-    console.log(`This is the random number: ${rando}`)
-    if (rando >= 0) {
-        powerArray.push(new Power(getRandomCoordinates(game.width),getRandomCoordinates(game.height),'black','stopper'))
-        console.log("pow pow")
-        console.log(powerArray)
-    }
-
+        powerArray.push(new Power(getRandomCoordinates(game.width-25),getRandomCoordinates(game.height-40)))
 }
 const detectPower = (thing) => {
     if (player.x < thing.x + thing.width
         && player.x + player.width > thing.x
         && player.y < thing.y + thing.height
         && player.y + player.height > thing.y) {
-            console.log('Boosted!')
-            timeStop()
+            // console.log('Boosted!')
+            if (thing.type === 'stopper') {
+                timeStop()
+            } else if (thing.type === 'pointer') {
+                morePoints()
+            } else if (thing.type === 'speedy') {
+                speedBoost()
+            } else if (thing.type === 'slowpoke') {
+                timeSlow()
+            } else if (thing.type === 'destroyer') {
+                destroy()
+            }
             powerArray.pop()
 
         }
 }
-///Stopping function, which is working. The timeouts are not. smallEnemies and seekers are the two arrays of enemies
+////Power up functions
+//Freeze Enemies
 const timeStop = () => {
     for (let i = 0; i<smallEnemies.length; i++) {
         smallEnemies[i].speed = 0
@@ -348,8 +421,40 @@ const timeStop = () => {
     setTimeout(resumeSpeed,5000,smallEnemies,(2+lvl))
     setTimeout(resumeSpeed,5000,seekers, lvl)
 }
+//Slow enemies
+const timeSlow = () => {
+    for (let i = 0; i<smallEnemies.length; i++) {
+        smallEnemies[i].speed /= 2
+    }
+    for (let i = 0; i<seekers.length; i++) {
+        seekers[i].speed /= 2
+    }
+    setTimeout(resumeSpeed,5000,smallEnemies,(2+lvl))
+    setTimeout(resumeSpeed,5000,seekers, lvl)
+    // console.log('tooslow')
+}
+//Boosts score
+const morePoints = () => {
+    point+=250
+    // console.log('cha-ching')
+}
+//Speeds Up player
+const speedBoost = () => {
+    player.speed += 5
+    setTimeout((function (){player.speed -=5}), 5000)
+    // console.log('greased')
+}
+
+//Destroys enemies
+const destroy = () => {
+    smallEnemies=[]
+    seekers=[]
+    // console.log('bye')
+}
 
 let point = 0
+let newHighScore = 0
+theHighScore.textContent = 0
 
 ///////////GAME LOOP ///////////
 const gameLoop = () => {
@@ -409,7 +514,29 @@ let enemyMove
 //     enemyMove = setInterval(setEnemyDirection, 1500)
 //     //setTimeout(lvl2,15000)
 // })
-
+highScoreList = []
+const highScoreOrder = (a,b) => {
+    return b-a
+}
+const recordScores= () => {
+    highScoreList.push(point)
+    highScoreList.sort(highScoreOrder)
+    // console.log(highScoreList)
+    const oldScores = document.getElementById('the-scores')
+    if (oldScores) {
+        oldScores.remove()
+    }
+    const ulScores = document.createElement('ul')
+    ulScores.id = 'the-scores'
+    for (let i = 0; i<5; i++) {
+        const liScore = document.createElement('li')
+        if (highScoreList[i]) {
+            liScore.textContent = highScoreList[i]
+        }
+        ulScores.appendChild(liScore)
+    }
+    scoreboard.appendChild(ulScores)
+}
 const stopGameLoop = () => { 
     clearInterval(gameInterval) 
     clearInterval(enemyMove)
@@ -417,39 +544,24 @@ const stopGameLoop = () => {
     clearInterval(releaseEnemy)
     clearInterval(releaseSeeker)
     clearInterval(newLevel)
+    gameOverScreen.style.display = 'flex'
+    finalScore.textContent=`You scored ${point} points`
+    if (point>newHighScore) {
+        newHighScore=point
+        theHighScore.textContent=`${newHighScore}`
+    }
+    recordScores()
 }
 
-//////Title Screen Functions///////
-const startGame = () => {
-    titleScreen.style.display = 'none'
-    gameInterval = setInterval(gameLoop, 50)
-    newEnemy(3)
-    setEnemyDirection()
-    releaseEnemy = setInterval(newEnemy,10000,(2+lvl))
-    releaseSeeker = setInterval(newSeeker,16000,lvl)
-    powerInterval = setInterval(createPower,18000)
-    enemyMove = setInterval(setEnemyDirection, 1500)
-    newLevel = setInterval(lvlUp,35000)
-    //setTimeout(lvl2,15000)
-}
-// const highlight = (event) => {
-//     event.target.style.color = 'white'
-// }
 
 
-startButton.addEventListener('click',startGame)
-// titleScreen.addEventListener('mouseover', highlight)
 
 ////////////////TO DO///////////////////////
-    // Fine tune intervals and levels
-    //// when are certain enemies released? how long is a level??
     
     ///////// Title Screen //////////
     // High scores
     ///// separate high score box that doesn't get cleared with a reset
     ///// how to save high score when closing and reloading page?
-    // Play Game
-    // Instructions/controls
 
     ///////// Beautify /////////////
     ///Nice background/////
